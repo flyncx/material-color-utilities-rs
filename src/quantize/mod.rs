b8f7e3bcffbcb69ecfdc3087ceefa73e6d8f7260
Flyncx <77253628+flyncx@users.noreply.github.com>
2024-03-26 13:48:55 +0700

    ported quantizer trait

--- src/quantize/mod.rs
+++ src/quantize/mod.rs
@@ -1,1 +1,2 @@
+pub mod quantizer;
 pub mod src;
---
+++ src/quantize/quantizer.rs
@@ -0,0 +1,22 @@
+use std::collections::HashMap;
+
+pub trait Quantizer {
+    fn quantize(pixels: Vec<i64>, max_colors: i64) -> QuantizerResult;
+}
+
+pub struct QuantizerResult {
+    pub color_to_count: HashMap<i64, i64>,
+    pub input_pixel_to_cluster_pixel: HashMap<i64, i64>,
+}
+impl QuantizerResult {
+    pub fn new(
+        color_to_count: HashMap<i64, i64>,
+        input_pixel_to_cluster_pixel: Option<HashMap<i64, i64>>,
+    ) -> QuantizerResult {
+        let input_pixel_to_cluster_pixel = input_pixel_to_cluster_pixel.unwrap_or(HashMap::new());
+        QuantizerResult {
+            color_to_count,
+            input_pixel_to_cluster_pixel,
+        }
+    }
+}
